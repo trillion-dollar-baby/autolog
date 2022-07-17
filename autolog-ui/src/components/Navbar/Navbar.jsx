66d37fbc74a2f29b9706790555b2cbdb1a6ec483
Enzo Falone <enzofalone123@gmail.com>
2022-07-17 19:43:39 -0400

    created barebones navbar

--- autolog-ui/src/components/Navbar/Navbar.jsx
+++ autolog-ui/src/components/Navbar/Navbar.jsx
@@ -4,22 +4,20 @@ import logoImage from '../../assets/red-car.png';
 import './Navbar.css'
 
 export default function Navbar() {
-    return (
-        <nav className='navbar'>
-            <div className="navbar-content">
-            <div className="section">
-            <Link to='/dashboard'>
-                <img className='logo-image' src={logoImage}/>
-                <span className="logo-title">Autolog</span>
-            </Link>
-            </div>
-            <div className="section">
-                {/* TODO: create a function to log out the user and lead to landing page or login screen */}
-            <NavLink className={(isActive) => {return 'btn-log-out'}}to={'/'} onClick={undefined}>
-                Log Out
-            </NavLink>
-            </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className='navbar'>
+      <div className="section">
+        <Link to='/dashboard'>
+          <img className='logo-image' src={logoImage} />
+          <span className="logo-title">Autolog</span>
+        </Link>
+      </div>
+      <div className="section">
+        {/* TODO: create a function to log out the user and lead to landing page or login screen */}
+        <NavLink className={(isActive) => { return 'btn-log-out' }} to={'/'} onClick={undefined}>
+          Log Out
+        </NavLink>
+      </div>
+    </nav>
+  )
 }
