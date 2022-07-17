a49b50953561ae8635848bf29c285e167d788ce6
Enzo Falone <enzofalone123@gmail.com>
2022-07-17 19:44:27 -0400

    implemented barebones sidebar

--- autolog-ui/src/components/Sidebar/Sidebar.jsx
+++ autolog-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,18 @@
 import * as React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Sidebar.css'
 
 export default function Sidebar() {
   return (
     <nav className='sidebar'>
-
+      <span className="sidebar title">
+        General
+      </span>
+      <NavLink className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} to='/dashboard/'>Dashboard</NavLink>
+      <NavLink className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} to='/performance/'>Performance</NavLink>
+      <NavLink className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} to='/inventory/'>Inventory</NavLink>
+      <NavLink className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} to='/settings/'>Settings</NavLink>
+      
     </nav>
   )
 }
