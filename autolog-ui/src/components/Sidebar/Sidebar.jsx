a7ea33f0955a6b32a4ab3495dee2e36b0677ad3e
Enzo Falone <enzofalone123@gmail.com>
2022-07-18 12:44:57 -0400

    fixed sidebar, created variables and added assets

--- autolog-ui/src/components/Sidebar/Sidebar.jsx
+++ autolog-ui/src/components/Sidebar/Sidebar.jsx
@@ -4,19 +4,27 @@ import './Sidebar.css'
 
 export default function Sidebar() {
   return (
-    <nav className='sidebar'>
-      <span className="sidebar title">
-        General
-      </span>
-      <NavLink className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} to='/dashboard/'>Dashboard</NavLink>
-      <NavLink className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} to='/performance/'>Performance</NavLink>
-      <NavLink className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} to='/inventory/'>Inventory</NavLink>
-      <NavLink className={({isActive}) => `sidebar-item ${isActive ? 'active' : ''}`} to='/settings/'>
-        
-        <span>Settings</span>
+    <div className='sidebar-empty'>
+      <nav className="sidebar">
+        <span className="title">
+          General
+        </span>
+        <NavLink className={({ isActive }) => `sidebar-item ${isActive ? 'active' : ''}`} to='/dashboard/'>
+          <img src="C:\Users\Enzo Falone\programming\autolog\autolog-ui\src\assets\dashboard.png" alt="icon" />
+        <span>Dashboard</span>
+        </NavLink>
+        <NavLink className={({ isActive }) => `sidebar-item ${isActive ? 'active' : ''}`} to='/performance/'>
+          <span>Performance</span>
+        </NavLink>
+        <NavLink className={({ isActive }) => `sidebar-item ${isActive ? 'active' : ''}`} to='/inventory/'>
+          <span>Inventory</span>
+        </NavLink>
+        <NavLink className={({ isActive }) => `sidebar-item ${isActive ? 'active' : ''}`} to='/settings/'>
 
-      </NavLink>
-      
-    </nav>
+          <span>Settings</span>
+
+        </NavLink>
+      </nav>
+    </div>
   )
 }
