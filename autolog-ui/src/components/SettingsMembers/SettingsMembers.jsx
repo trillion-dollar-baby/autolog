80af0a3d9b3d5a43e011c2671493c7b158d7c495
Enzo Falone <enzofalone123@gmail.com>
2022-07-19 15:10:44 -0400

    added dummy data

--- autolog-ui/src/components/SettingsMembers/SettingsMembers.jsx
+++ autolog-ui/src/components/SettingsMembers/SettingsMembers.jsx
@@ -7,20 +7,53 @@ import MemberList from '../MemberList/MemberList';
 import Dropdown from '../Dropdown/Dropdown';
 
 export default function SettingsMembers() {
-  const roleOptions = [
-    'Admin',
-    'Manager',
-    'Employee',
-    'Viewer'
+  const data = {
+    0: 'admin',
+    1: 'manager',
+    2: 'employee',
+    3: 'viewer'
+  }
+
+  const [userRoles, setUserRoles] = useState(data);
+
+  const userArray = [
+    {
+      id: 0,
+      firstName: 'enzo',
+      lastName: 'falone',
+      email: 'enzo@falone.io',
+      role: 'admin'
+    },
+    {
+      id: 1,
+      firstName: 'enzo2',
+      lastName: 'falone2',
+      email: 'enzo2@falone.io',
+      role: 'manager'
+    },
+    {
+      id: 2,
+      firstName: 'enzo3',
+      lastName: 'falone3',
+      email: 'enzo3@falone.io',
+      role: 'employee'
+    },
+    {
+      id: 3,
+      firstName: 'enzo4',
+      lastName: 'falone4',
+      email: 'enzo4@falone.io',
+      role: 'viewer'
+    }
   ]
-  const [value, setValue] = useState(roleOptions[1]);
+
+
 
   return (
     <div>
       <div className="settings settings-members">
-        <MemberList/>
+        <MemberList userArray={userArray} userRoles={userRoles} setUserRoles={setUserRoles}/>
       </div>
-      {/* <Dropdown title={'hello world'}/> */}
       
       {/* <div className='settings-divider'>
                 <span>Password settings</span>
