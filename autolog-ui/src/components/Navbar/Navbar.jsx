52ba01a1f3a6215f3fe6cc19455b0c58e5144873
Enzo Falone <enzofalone123@gmail.com>
2022-07-19 18:19:01 -0400

    Created styling and buttons for login, signup and logout redirects ready to be connected with backend related functions

--- autolog-ui/src/components/Navbar/Navbar.jsx
+++ autolog-ui/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
+import { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logoImage from '../../assets/red-car.png';
 import './Navbar.css'
 
 export default function Navbar() {
+
+  //testing useState, remove this and check if user is logged in 
+  const [login, setLogin] = useState(false);
+
   return (
     <nav className='navbar'>
       <div className="section">
@@ -12,12 +17,23 @@ export default function Navbar() {
           <span className="logo-title">Autolog</span>
         </Link>
       </div>
-      <Link to='/Register'> Login </Link>
       <div className="section">
-        {/* TODO: create a function to log out the user and lead to landing page or login screen */}
-        <NavLink className={(isActive) => { return 'btn-log-out' }} to={'/'} onClick={undefined}>
-          Log Out
-        </NavLink>
+        {(login ?
+        <>
+          {/* TODO: create dropdown showing what inventories does the user has */}
+          {/* TODO: create a function to log out the user and lead to landing page or login screen */}
+          <NavLink className={(isActive) => 'btn-log-out'} to={'/'} onClick={() => setLogin(false)}>
+            Log Out
+          </NavLink>
+        </>
+        :
+        <>
+          <Link to='/login'>Login</Link>
+          <NavLink className={(isActive) => 'btn-sign-up'} to={'/register'}>
+            Sign Up
+          </NavLink>
+        </>
+        )}
       </div>
     </nav>
   )
