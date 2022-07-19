41e6449138131a9e17872089618137398769c752
Enzo Falone <enzofalone123@gmail.com>
2022-07-19 15:09:49 -0400

    added dummy data and set everything to receive an array of data from backend

--- autolog-ui/src/components/MemberItem/MemberItem.jsx
+++ autolog-ui/src/components/MemberItem/MemberItem.jsx
@@ -1,56 +1,41 @@
 import './MemberItem.css'
 
+import _ from 'lodash';
+
 import placeholderImage from '../../assets/placeholder.jpg';
 import iconDelete from '../../assets/icons8-delete.png';
 import DropdownSmall from '../DropdownSmall/DropdownSmall';
 import { useState } from 'react';
+import Dropdown from '../Dropdown/Dropdown';
 
-/**
- * 
- * 
- */
+export default function MemberItem({index, id, firstName, lastName, email, userRole, setUserRole}) {
+  
+  const roleOptions = [
+    'admin',
+    'manager',
+    'employee',
+    'viewer'
+  ]
+  
+  // get role value from the dropdown and change it to 
+  const onDropdownClick = (role) => {
+    // change member role
+    setUserRole(id, role);
+  }
 
-export default function MemberItem() {
-    const roleOptions = [
-        'Admin',
-        'Manager',
-        'Employee',
-        'Viewer'
-    ]
-    const [value, setValue] = useState(roleOptions[1]);
-
-    return (
-
-        <>
-            {/* TODO: make component to be populated by data received */}
-            {/* test with different components */}
-            <div className="member-item first">
-                <img className='member-item-image' src={placeholderImage} />
-                <div className="member-item-name">
-                    <span>Saul Goodman (saulGoodman123)</span>
-                    <span>saul@goodman.io</span>
-                </div>
-                <DropdownSmall value={value} setValue={setValue} options={roleOptions} />
-                <img className={'member-item-delete'} src={iconDelete} />
-            </div>
-            <div className="member-item">
-                <img className='member-item-image' src={placeholderImage} />
-                <div className="member-item-name">
-                    <span>Saul Goodman (saulGoodman123)</span>
-                    <span>saul@goodman.io</span>
-                </div>
-                <DropdownSmall value={value} setValue={setValue} options={roleOptions} />
-                <img className={'member-item-delete'} src={iconDelete} />
-            </div>
-            <div className="member-item last">
-                <img className='member-item-image' src={placeholderImage} />
-                <div className="member-item-name">
-                    <span>Saul Goodman (saulGoodman123)</span>
-                    <span>saul@goodman.io</span>
-                </div>
-                <DropdownSmall value={value} setValue={setValue} options={roleOptions} />
-                <img className={'member-item-delete'} src={iconDelete} />
-            </div>
-        </>
-    )
+  return (
+    <div className={`member-item ${index}`}>
+      <div className="section">
+        <img className='member-item-image' src={placeholderImage} />
+        <div className="member-item-name">
+          <span>{_.capitalize(firstName)} {_.capitalize(lastName)}</span>
+          <span>{email}</span>
+        </div>
+      </div>
+      <div className='section'>
+        <Dropdown value={userRole} onSelect={onDropdownClick} items={roleOptions} />
+        <img className={'member-item-delete'} title='Remove member from inventory' alt='remove button' src={iconDelete} />
+      </div>
+    </div>
+  )
 }
