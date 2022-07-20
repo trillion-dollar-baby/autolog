d96a9ef7cf953480d7ac224e9427b2881faa68b3
unknown <chen.deland@gmail.com>
2022-07-20 14:18:22 -0400

    Fixed table CSS and added to inventory component

--- autolog-ui/src/components/Inventory/Inventory.jsx
+++ autolog-ui/src/components/Inventory/Inventory.jsx
@@ -1,10 +1,31 @@
 import './Inventory.css'
+import Table from '../Table/Table'
+import Topbar from '../Topbar/Topbar'
 
 export default function Inventory() {
     //TODO: implement
-    return (
-        <div className="inventory">
+    const testCol = ["ID", "Part", "Quantity"]
+    const testArr = [{ID: 5, Part: "hammer", Quantity: 5}, {ID: 6, Part: "screw", Quantity: 10}, {ID: 15, Part: "table", Quantity: 3}]
 
+    const settingsRoutes = [
+        {
+          name: 'Inventory',
+          to: './'
+        },
+        {
+          name: 'Orders',
+          to: './orders'
+        }
+      ]
+
+    return (
+        <div className='inventory-content'>
+            <div className='topbar-container'>
+                <Topbar routes={settingsRoutes} />
+            </div>
+            <div className='table-container'>
+                <Table tableElementArray={testArr} tableColumnLabelArray={testCol} />
+            </div>
         </div>
     )
 }
