85593d07e5168fa80e1271a71533baaac4da756f
unknown <chen.deland@gmail.com>
2022-07-21 17:09:39 -0400

    inventory context functions

--- autolog-ui/src/contexts/inventory.jsx
+++ autolog-ui/src/contexts/inventory.jsx
@@ -10,6 +10,60 @@ export const InventoryContextProvider = ({ children }) => {
   const [inventoryMembers, setInventoryMembers] = useState([]);
   const [error, setError] = useState("");
   
+  // Create an inventory
+const createInventory = async (values) => {
+    const {data, error} = await apiClient.createInventory(values);
+    if (!error) {
+        console.log("Created inventory is:", data);
+       }
+       else {
+        console.error("Error acreating inventory, message:", error)
+       }
+}
+
+// Get inventories that are accessible to the user
+const getAccessibleInventories = async () => {
+   const { data, error } = await apiClient.getAccessibleInventories();
+   if (!error) {
+    setInventoryMembers(data);
+   }
+   else {
+    console.error("Error getting accessible inventories, message:", error)
+   }
+}
+
+// get inventories owned by the user
+const getOwnedInventories = async () => {
+   const { data, error } = await apiClient.getOwnedInventories();
+   if (!error) {
+    setOwnedInventories(data);
+   }
+   else {
+    console.error("Error getting owned inventories, message:", error)
+   }
+}
+
+// Get members of a given inventory
+const getInventoryMembers = async (inventoryId) => {
+   const { data, error } = await apiClient.getInventoryMembers(inventoryId);
+   if (!error) {
+    setInventoryMembers(data);
+   }
+   else {
+    console.error("Error getting inventory members, message:", error)
+   }
+}
+
+// Add members to an inventory
+const addInventoryMembers = async (userEmail, InventoryId) => {
+   const { data, error } = await apiClient.addInventoryMember(userEmail, InventoryId);
+   if (!error) {
+    console.log("Added member is", data);
+   }
+   else {
+    console.error("Error adding member to inventory, message:", error)
+   }
+}
 
   return (
     <InventoryContext.Provider value={{
@@ -21,24 +75,4 @@ export const InventoryContextProvider = ({ children }) => {
       {children}
     </InventoryContext.Provider>
   )
-}
-
-const createInventory = async (values) => {
-    const {data, error} = await apiClient.createInventory(values);
-}
-
-const getAccessibleInventories = async () => {
-    const { data, error } = await apiClient.getAccessibleInventories();
-}
-
-const getOwnedInventories = async () => {
-    const { data, error } = await apiClient.getOwnedInventories();
-}
-
-const getInventoryMembers = async (inventoryId) => {
-    const { data, error } = await apiClient.getInventoryMembers(inventoryId);
-}
-
-const addInventoryMembers = async (userEmail, InventoryId) => {
-    const { data, error } = await apiClient.addInventoryMember(userEmail, InventoryId);
 }
