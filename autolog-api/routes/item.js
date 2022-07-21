da3c47a0c2aff076803ffe4b17fe96f84e31c55a
wyneep <wp2279@barnard.edu>
2022-07-21 13:12:08 -0400

    barebones for item models

--- autolog-api/routes/item.js
+++ autolog-api/routes/item.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get("/",  async (req, res, next) => {
     try {
-        //send json response to client with all of the user-owned nutrition instances in an array
+        //send json response to client with all of the user-owned item instances in an array
         const { user } = res.locals
         const items = await Item.listItemForUser(user)
         return res.status(201).json({ items })
@@ -19,24 +19,25 @@ router.post("/", security.requireAuthenticatedUser, async (req, res, next) => {
     try {
         
         const { user } = res.locals
-        const items = await Item.createItem({items: req.body, user})
+        console.log(user)
+        const items = await Item.createItem({item: req.body, user: user})
         return res.status(201).json({ items })
     }
     catch(err) {
         next(err)
     }
 })
-router.get("/id/:itemId", async (req, res, next) => {
-    try {
-        const { itemId } = req.params.itemId
-        const items = await Item.fetchItemById(itemId)
-        console.log(JSON.stringify(items))
-        return res.status(200).json({ items })
-    }
-    catch(err) {
-        next(err)
-    }
-})
+// router.get("/id/:itemId", async (req, res, next) => {
+//     try {
+//         const { itemId } = req.params.itemId
+//         const items = await Item.fetchItemById(itemId)
+//         console.log(JSON.stringify(items))
+//         return res.status(200).json({ items })
+//     }
+//     catch(err) {
+//         next(err)
+//     }
+// })
 
 
 module.exports = router
