48a5297e4c9014dc822bd6054797727b45cbcfcf
unknown <chen.deland@gmail.com>
2022-07-21 13:21:26 -0400

    created inventory search bar and fixed reusable dropdown + form

--- autolog-ui/src/components/Inventory/Inventory.jsx
+++ autolog-ui/src/components/Inventory/Inventory.jsx
@@ -1,6 +1,8 @@
 import './Inventory.css'
 import Table from '../Table/Table'
 import Topbar from '../Topbar/Topbar'
+import Dropdown from '../Dropdown/Dropdown'
+import FormInput from '../FormInput/FormInput'
 
 export default function Inventory() {
     //TODO: implement
@@ -18,10 +20,24 @@ export default function Inventory() {
         }
       ]
 
+    const nameFilters = ["A", "B", "C"]
+    const categoryFilter = ["Tools", "Parts", "Cars"]
+
     return (
         <div className='inventory-content'>
             <div className='topbar-container'>
                 <Topbar routes={settingsRoutes} buttonName={"Create"} buttonPath={"./create"} />
+            </div>
+            <div className='filter-container'>
+              <div className='search-bar'>
+                <FormInput data={{name: "search", type: "text", placeholder: "Search for items..."}} inputValue={null} onChange={null} />
+              </div>
+              <div className='filter-by-name'>
+                <Dropdown items={nameFilters} value={"Sort by name"}/>
+              </div>
+              <div className='filter-by-category'>
+                <Dropdown items={categoryFilter} value={"Sort by category"}/>
+              </div>
             </div>
             <div className='table-container'>
                 <Table tableElementArray={testArr} tableColumnLabelArray={testCol}/>
