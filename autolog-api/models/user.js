724b53f120e802d44ebb8669b8b5cc1f1f1b9539
Enzo Falone <enzofalone123@gmail.com>
2022-07-22 13:26:39 -0400

    added PATCH routes and models to change user settings and password

--- autolog-api/models/user.js
+++ autolog-api/models/user.js
@@ -1,7 +1,7 @@
-const bcrypt = require("bcrypt")
-const { BCRYPT_WORK_FACTOR } = require("../config")
-const db = require("../db")
-const { BadRequestError, UnauthorizedError } = require("../utils/errors")
+const bcrypt = require("bcrypt");
+const { BCRYPT_WORK_FACTOR } = require("../config");
+const db = require("../db");
+const { BadRequestError, UnauthorizedError } = require("../utils/errors");
 
 class User {
   static makePublicUser(user) {
@@ -11,65 +11,82 @@ class User {
       username: user.username,
       firstName: user.first_name,
       lastName: user.last_name,
+      phoneNumber: user.phone_number,
       createdAt: user.created_at,
-    }
+    };
   }
 
   static async login(credentials) {
-    const requiredFields = ["email", "password"]
+    const requiredFields = ["email", "password"];
     requiredFields.forEach((property) => {
       if (!credentials?.hasOwnProperty(property)) {
-        throw new BadRequestError(`Missing ${property} in request body.`)
+        throw new BadRequestError(`Missing ${property} in request body.`);
       }
-    })
+    });
 
-    const user = await User.fetchUserByEmail(credentials.email)
+    const user = await User.fetchUserByEmail(credentials.email);
     if (user) {
-      const isValid = await bcrypt.compare(credentials.password, user.password)
+      const isValid = await bcrypt.compare(credentials.password, user.password);
       if (isValid) {
-        return User.makePublicUser(user)
+        return User.makePublicUser(user);
       }
     }
 
-    throw new UnauthorizedError("Invalid username/password")
+    throw new UnauthorizedError("Invalid username/password");
   }
 
   static async register(credentials) {
-    const requiredFields = ["email", "username", "firstName", "lastName", "password", "phoneNumber"]
+    const requiredFields = [
+      "email",
+      "username",
+      "firstName",
+      "lastName",
+      "password",
+      "phoneNumber",
+    ];
     requiredFields.forEach((property) => {
       if (!credentials?.hasOwnProperty(property)) {
-        throw new BadRequestError(`Missing ${property} in request body.`)
+        throw new BadRequestError(`Missing ${property} in request body.`);
       }
-    })
+    });
 
     if (credentials.email.indexOf("@") <= 0) {
-      throw new BadRequestError("Invalid email.")
+      throw new BadRequestError("Invalid email.");
     }
 
-    const existingUser = await User.fetchUserByEmail(credentials.email)
+    const existingUser = await User.fetchUserByEmail(credentials.email);
     if (existingUser) {
-      throw new BadRequestError(`A user already exists with email: ${credentials.email}`)
+      throw new BadRequestError(
+        `A user already exists with email: ${credentials.email}`
+      );
     }
 
-    const existingUserWithUsername = await User.fetchUserByUsername(credentials.username)
+    const existingUserWithUsername = await User.fetchUserByUsername(
+      credentials.username
+    );
     if (existingUserWithUsername) {
-      throw new BadRequestError(`A user already exists with username: ${credentials.username}`)
+      throw new BadRequestError(
+        `A user already exists with username: ${credentials.username}`
+      );
     }
 
-    const hashedPassword = await bcrypt.hash(credentials.password, BCRYPT_WORK_FACTOR)
-    const normalizedEmail = credentials.email.toLowerCase()
-    const normalizedUsername = credentials.username.toLowerCase()
-    const firstName = credentials.firstName
-    const lastName = credentials.lastName
-    const phoneNumber = credentials.phoneNumber
-    const role = "admin"
+    const hashedPassword = await bcrypt.hash(
+      credentials.password,
+      BCRYPT_WORK_FACTOR
+    );
+    const normalizedEmail = credentials.email.toLowerCase();
+    const normalizedUsername = credentials.username.toLowerCase();
+    const firstName = credentials.firstName;
+    const lastName = credentials.lastName;
+    const phoneNumber = credentials.phoneNumber;
+    const role = "admin";
     const isVerified = true;
 
     const userResult = await db.query(
       `INSERT INTO users (email, username, first_name, last_name, password, phone_number, role, is_verified)
-       VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-       RETURNING id, email, username, first_name, last_name, created_at;
-      `,
+	   VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+	   RETURNING id, email, username, first_name, last_name, phone_number, created_at;
+	  `,
       [
         normalizedEmail,
         normalizedUsername,
@@ -78,24 +95,24 @@ class User {
         hashedPassword,
         phoneNumber,
         role,
-        isVerified
+        isVerified,
       ]
-    )
-    const user = User.makePublicUser(userResult.rows[0])
+    );
+    const user = User.makePublicUser(userResult.rows[0]);
 
-    return user
+    return user;
   }
 
   static async fetchUserByEmail(email) {
     if (!email) {
-      throw new BadRequestError("No email provided")
+      throw new BadRequestError("No email provided");
     }
 
-    const query = `SELECT * FROM users WHERE email = $1`
+    const query = `SELECT * FROM users WHERE email = $1`;
 
-    const result = await db.query(query, [email.toLowerCase()])
+    const result = await db.query(query, [email.toLowerCase()]);
 
-    const user = result.rows[0]
+    const user = result.rows[0];
 
     if (user) {
       return user;
@@ -106,14 +123,14 @@ class User {
 
   static async fetchUserByUsername(username) {
     if (!username) {
-      throw new BadRequestError("No username provided")
+      throw new BadRequestError("No username provided");
     }
 
-    const query = `SELECT * FROM users WHERE username = $1`
+    const query = `SELECT * FROM users WHERE username = $1`;
 
-    const result = await db.query(query, [username.toLowerCase()])
+    const result = await db.query(query, [username.toLowerCase()]);
 
-    const user = result.rows[0]
+    const user = result.rows[0];
 
     if (user) {
       return user;
@@ -121,6 +138,55 @@ class User {
 
     return false;
   }
+
+  /**
+   * Update user functions
+   */
+
+  static async updateCredentials(userId, newCredentials, credentials) {
+    // with old credentials loop through newCredentials object and replace
+    for (const key in newCredentials) {
+      credentials[key] = newCredentials[key];
+    }
+
+    // after replacing, update
+    const query = `
+			UPDATE users
+			SET first_name = $2,
+				last_name = $3,
+				email = $4,
+				username = $5,
+				phone_number = $6,
+				updated_at = NOW()
+			WHERE id = $1
+			RETURNING *;
+		`;
+
+    const result = await db.query(query, [
+      userId,
+      credentials.firstName,
+      credentials.lastName,
+      credentials.email,
+      credentials.username,
+      credentials.phoneNumber,
+    ]);
+
+    //   return result.rows[0];
+  }
+
+  static async updatePassword(userId, newCredential) {
+    const hashedPassword = await bcrypt.hash(
+      newCredential.password,
+      BCRYPT_WORK_FACTOR
+    );
+
+    const query = `
+		UPDATE users
+		SET password = $2
+		WHERE id = $1
+	`;
+    const result = await db.query(query, [userId, hashedPassword]);
+  }
 }
 
-module.exports = User
+module.exports = User;
