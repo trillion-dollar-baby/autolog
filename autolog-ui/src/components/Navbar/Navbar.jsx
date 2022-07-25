a19d61a125c60918203a5901aa874375ab286cd7
Enzo Falone <enzofalone123@gmail.com>
2022-07-25 12:55:27 -0400

    Navbar now checks if user is authenticated by checking token

--- autolog-ui/src/components/Navbar/Navbar.jsx
+++ autolog-ui/src/components/Navbar/Navbar.jsx
@@ -9,12 +9,14 @@ import InventoryDropdown from '../InventoryDropdown/InventoryDropdown';
 import './Navbar.css'
 
 export default function Navbar({login}) {
-  const {userContext, authContext} = useContext(AuthContext);
-  const [user, setUser] = userContext;
+  const {authContext} = useContext(AuthContext);
   const [loginUser, registerUser, logoutUser] = authContext;
-
+  
+  // flag variable
+  const isAuthenticated = apiClient.getToken();
+  
   // check on every render where the logo should redirect users to
-  const logoRedirect = apiClient?.token ? '/dashboard' : '/';
+  const logoRedirect = isAuthenticated ? '/dashboard' : '/';
 
   return (
     <nav className='navbar'>
@@ -28,7 +30,7 @@ export default function Navbar({login}) {
       {/* auth and inventory dropdown section */}
       <div className="section">
         {/* Render buttons depending on whether user is logged in or not */}
-        {(apiClient?.token ?
+        {(isAuthenticated ?
         <>
           <InventoryDropdown/>
           <NavLink className={(isActive) => 'btn-log-out'} to={'/login'} onClick={() => logoutUser()}>
