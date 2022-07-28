6119c588c4fabfa451908f726d95455443a963a9
wyneep <wp2279@barnard.edu>
2022-07-28 14:47:04 -0400

    search function complete

--- autolog-ui/src/components/Inventory/Inventory.jsx
+++ autolog-ui/src/components/Inventory/Inventory.jsx
@@ -1,66 +1,111 @@
-import * as React from 'react';
-import { useContext } from 'react';
-import './Inventory.css'
-import Table from '../Table/Table'
-import Topbar from '../Topbar/Topbar'
-import Dropdown from '../Dropdown/Dropdown'
-import FormInput from '../FormInput/FormInput'
-import ItemContext from '../../contexts/items';
-import InventoryContext from '../../contexts/inventory';
-import Loading from '../Loading/Loading';
+import * as React from "react";
+import { useState } from "react";
+import { useContext } from "react";
+import "./Inventory.css";
+import Table from "../Table/Table";
+import Topbar from "../Topbar/Topbar";
+import Dropdown from "../Dropdown/Dropdown";
+import FormInput from "../FormInput/FormInput";
+import ItemContext from "../../contexts/items";
+import InventoryContext from "../../contexts/inventory";
+import Loading from "../Loading/Loading";
 
 export default function Inventory() {
-    //TODO: implement
-    const { processingContext, initializedContext } = useContext(InventoryContext)
-    const [ isProcessing, setIsProcessing ] = processingContext
-    const [ initialized, setInitialized ] = initializedContext
-    const { itemContext } = useContext(ItemContext)
-    const [ items, setItems ] = itemContext
+  const { processingContext, initializedContext } =
+    useContext(InventoryContext);
+  const [isProcessing, setIsProcessing] = processingContext;
+  const [initialized, setInitialized] = initializedContext;
+  const { itemContext } = useContext(ItemContext);
+  const [items, setItems] = itemContext;
+  const [value, setValue] = useState("");
+  const { searchContext } = useContext(ItemContext);
+  const [searchItem] = searchContext;
 
-    console.log(items);
+  const settingsRoutes = [
+    {
+      name: "Inventory",
+      to: "./",
+    },
+    {
+      name: "Orders",
+      to: "/item/create",
+    },
+  ];
 
-    const settingsRoutes = [
-        {
-          name: 'Inventory',
-          to: './'
-        },
-        {
-          name: 'Orders',
-          to: '/item/create'
-        }
-      ]
+  const nameFilters = ["A", "B", "C"];
+  const categoryFilter = ["Tools", "Parts", "Cars"];
 
-    const nameFilters = ["A", "B", "C"]
-    const categoryFilter = ["Tools", "Parts", "Cars"]
-    
-    const onChange = (event) => {
-      setItems(event.target.value)
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+  async function handle(e, searchTerm) {
+    // setValue(searchTerm);
+    if (e.key === "Enter") {
+      e.preventDefault();
+      searchTerm = value;
+      setIsProcessing(true);
+      const result = await searchItem(searchTerm, 0);
+      setIsProcessing(false);
+      //reset page number
+      // setPageNumber(page)
 
+      console.log("search", searchItem);
+
+      setItems(result.items);
+      console.log(result.items);
     }
-    function handle(searchTerm){
-      console.log("search", searchTerm)
-    }
+    console.log("no search currently");
+  }
 
-    return (
-        <div className='inventory-content'>
-            <div className='topbar-container'>
-                <Topbar routes={settingsRoutes} buttonName={"Create"} buttonPath={"/item/create"} />
-            </div>
-            <div className='filter-container'>
-              <div className='search-bar'>
-                <FormInput data={{name: "search", type: "text", placeholder: "Search for items...", onkeypress: "handle(event)"}} inputValue={items.value} onChange={onChange} />
-              </div>
-              <div className='filter-by-name'>
-                <Dropdown items={nameFilters} value={"Sort by name"}/>
-              </div>
-              <div className='filter-by-category'>
-                <Dropdown items={categoryFilter} value={"Sort by category"}/>
-              </div>
-            </div>
-            <div className='table-container'>
-                {(isProcessing || !initialized) ? <Loading /> :
-                <Table tableLabel={"Results"} tableElementArray={(items.length) ? items : []} tableColumnLabelArray={(items.length) ? Object.keys(items[0]) : []}/>}
-            </div>
+  // function search(rows){
+  //   const columns = rows[0] && Object.keys(rows[0]);
+  //   return rows.filter((row)=>
+  //   columns.some(
+  //     (column) => row[column].toString().toLowerCase().indexOf(value.toLowerCase()) > -1
+  //   )
+  //   );
+  // }
+
+  return (
+    <div className="inventory-content">
+      <div className="topbar-container">
+        <Topbar
+          routes={settingsRoutes}
+          buttonName={"Create"}
+          buttonPath={"/item/create"}
+        />
+      </div>
+      <div className="filter-container">
+        <div className="search-bar">
+          <FormInput
+            data={{
+              name: "search",
+              type: "text",
+              placeholder: "Search for items...",
+            }}
+            inputValue={value}
+            onChange={onChange}
+            onkeypress={handle}
+          />
         </div>
-    )
+        <div className="filter-by-name">
+          <Dropdown items={nameFilters} value={"Sort by name"} />
+        </div>
+        <div className="filter-by-category">
+          <Dropdown items={categoryFilter} value={"Sort by category"} />
+        </div>
+      </div>
+      <div className="table-container">
+        {isProcessing || !initialized ? (
+          <Loading />
+        ) : (
+          <Table
+            tableLabel={"Results"}
+            tableElementArray={items.length ? items : []}
+            tableColumnLabelArray={items.length ? Object.keys(items[0]) : []}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
--- autolog-ui/src/contexts/items.jsx
+++ autolog-ui/src/contexts/items.jsx
@@ -1,77 +1,93 @@
-import { createContext, useState, useEffect, useContext } from "react";
-import apiClient from "../services/apiClient";
-import InventoryContext from "./inventory";
-import AuthContext from "./auth";
+import { createContext, useState, useEffect, useContext} from "react"
+import apiClient from "../services/apiClient"
+import InventoryContext from "./inventory"
+import AuthContext from "./auth"
+import Loading from "../components/Loading/Loading"
 
 const ItemContext = createContext({});
-export const ItemContextProvider = ({ children }) => {
-  const [items, setItems] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+export const ItemContextProvider = ({children})=>{
+    const [items, setItems] =useState([]);
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    
+//should call the useAuthContext
+const { userContext } = useContext(AuthContext);
+const [user, setUser] = userContext;
+//connect inventory context
+const {accessibleInventoriesContext} = useContext(InventoryContext);
+const [accessibleInventories, setAccessibleInventories]= accessibleInventoriesContext;
 
-  //should call the useAuthContext
-  const { userContext } = useContext(AuthContext);
-  const [user, setUser] = userContext;
-  //connect inventory context
-  const { accessibleInventoriesContext } = useContext(InventoryContext);
-  const [accessibleInventories, setAccessibleInventories] =
-    accessibleInventoriesContext;
+const {selectedInventoryContext}= useContext(InventoryContext);
+const [selectedInventory, setSelectedInventory] = selectedInventoryContext
 
-  const { selectedInventoryContext } = useContext(InventoryContext);
-  const [selectedInventory, setSelectedInventory] = selectedInventoryContext;
 
-  useEffect(() => {
-      const fetchItemList = async () => {
-        setIsLoading(true);
+ useEffect(()=>{
 
-        const { data, err } = await apiClient.getItemList(
-          selectedInventory?.inventoryId,
-          0,
-          ""
-        );
-
-        if (data) {
-          setItems(data?.items);
-        } else if (err) {
-          setError(err);
+    if (user) {
+      async function fetchItemList(){
+        setIsLoading(true)
+        
+        const {data, err} = await apiClient.getItemList(selectedInventory?.inventoryId, 0, '')
+        
+        if(data){
+            setItems(data?.items) 
+    
+        }else if(err){
+    
+        setError(err)
+       
         }
-        setIsLoading(false);
-      }
+        setIsLoading(false)
+        }
+        fetchItemList()
+    }
 
-      if (user && selectedInventory?.inventoryId) {
-        fetchItemList();
-      }
-  }, [user, selectedInventory]);
+   }, [user, selectedInventory])
 
-  // Get id of a given item
-  // for when we are accessing the item through item details
-  // const getItem = async (itemId) => {
-  //   const { data, error } = await apiClient.getItem(itemId);
-  //   if (!error) {
-  //     console.log("Items are:", data);
-  //   } else {
-  //     console.error("Error getting items, message:", error);
-  //   }
-  // };
+   // Get id of a given item
+   //for when we are accessing the item through item details
+  const getItem = async (itemId) => {
+    const { data, error } = await apiClient.getItem(itemId);
+    if (!error) {
+      console.log("Items are:", data);
+    }
+    else {
+      console.error("Error getting items, message:", error)
+    }
+  }
 
-  // Create item
+  //create item
   const createItem = async (values) => {
     const { data, error } = await apiClient.createItem(values);
     if (!error) {
-      // console.log("Created item is:", data);
-    } else {
-      console.error("Error creating items, message:", error);
+      console.log("Created item is:", data);
     }
-  };
+    else {
+      console.error("Error creating items, message:", error)
+    }
+  }
 
-  const values = {
-    errorContext: [error, setError],
-    itemContext: [items, setItems],
-    loadingContext: [isLoading, setIsLoading],
-    itemCreateContext: [createItem],
-  };
+  //search item
 
-  return <ItemContext.Provider value={values}>{children}</ItemContext.Provider>;
-};
+  const searchItem = async(search, pageNumber)=>{
+    const {data, error} = await apiClient.getItemList(selectedInventory?.inventoryId, pageNumber, search);
+    if(!error){
+      console.log("Searched item is:", data);
+      return data;
+      
+    }
+    else{
+      console.log("Error searching for items, message:", error)
+    }
+  }
+
+const values={errorContext: [error, setError], itemContext: [items, setItems], loadingContext: [isLoading, setIsLoading], itemCreateContext: [createItem], searchContext:[searchItem]}
+   
+return(
+        <ItemContext.Provider value={values}>
+            {children}
+        </ItemContext.Provider>
+    )
+}
 
 export default ItemContext;
