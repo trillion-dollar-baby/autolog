4e8416bfe8ce8a29e38f0b006e64809603e3e012
Enzo Falone <enzofalone123@gmail.com>
2022-07-28 14:43:46 -0400

    updated styling

--- autolog-ui/src/components/ModalButton/ModalButton.jsx
+++ autolog-ui/src/components/ModalButton/ModalButton.jsx
@@ -6,7 +6,7 @@ import {motion} from 'framer-motion'
 
 function ModalButton({children, style,onClick}) {
   return (
-    <motion.button style={style} whileTap={{scale:0.95}} onClick={onClick}>
+    <motion.button className={'modal-button'} style={style} whileTap={{scale:0.95}} onClick={onClick}>
         {children}
     </motion.button>
   )
