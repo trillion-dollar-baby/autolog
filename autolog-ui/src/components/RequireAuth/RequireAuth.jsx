9a7b9946b54dea20b601fba33547f61833f5ac5d
Enzo Falone <enzofalone123@gmail.com>
2022-08-01 11:49:55 -0400

    Lead user to /inventory/create if no inventories

--- autolog-ui/src/components/RequireAuth/RequireAuth.jsx
+++ autolog-ui/src/components/RequireAuth/RequireAuth.jsx
@@ -1,13 +1,18 @@
 import { useContext } from "react";
 import { Navigate, useLocation } from "react-router";
 import AuthContext from "../../contexts/auth";
+import InventoryContext from "../../contexts/inventory";
 import apiClient from "../../services/apiClient";
 
 export default function RequireAuth({ children }) {
     const { userContext, errorContext } = useContext(AuthContext);
-    const location = useLocation();
     const [user, setUser] = userContext;
     const [error, setError] = errorContext;
+    
+    const {accessibleInventoriesContext} = useContext(InventoryContext);
+    const [accessibleInventories, setAccessibleInventories] = accessibleInventoriesContext;
+
+    const location = useLocation();
 
     const notAuthorized = () => {
         // make user know that he is unauthorized
@@ -15,8 +20,14 @@ export default function RequireAuth({ children }) {
         return <Navigate to='/login' replace state={{path: location.pathname}}/>
     }
 
-    // if an user email is present in AuthContext, render page
-    // if not, redirect to login page with an error message
+    // check if user has an inventory
+    if(apiClient.getToken() && (accessibleInventories.length < 1) && (location.pathname !== '/inventory/create')) {
+        return <Navigate to='/inventory/create' replace state={{path:location.pathname}}/>
+    }
+
+    // TODO: check if user is verificated when feature is fully working
+
+    // if all conditions are false, show application
     return (apiClient.getToken() ? 
         children 
         : 
